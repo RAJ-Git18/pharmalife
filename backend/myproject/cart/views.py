4a751db6767d119fa071a780b462a10f05113e42
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-02-21 13:16:39 +0545

    feat: django cart app

--- backend/myproject/cart/views.py
+++ backend/myproject/cart/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import HttpResponse
 
 # Create your views here.
+@api_view(['GET'])
+def get_cart_items(request):
+    return Response("add the cart to the cart items")
