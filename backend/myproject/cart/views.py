b989f4270047b036dba741c29f79fc9cc50424bc
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-02-24 21:32:03 +0545

    added the admin site

--- backend/myproject/cart/views.py
+++ backend/myproject/cart/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import HttpResponse
+from .serializer import CartItemSerializer
+from .models import CartItem
 
 # Create your views here.
-@api_view(['GET'])
-def get_cart_items(request):
-    return Response("add the cart to the cart items")
+# @api_view(['GET'])
+# def get_cart_items(request):
+#     items = CartItem.objects.all()
+#     serializer = CartItemSerializer(items, ma)
