8ced9c60875aefb10e5fd96f55d43029827fde5c
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-03-12 23:43:30 +0545

    login funcationality

--- backend/pharmaproject/login/views.py
+++ backend/pharmaproject/login/views.py
@@ -4,18 +4,24 @@ from django.contrib.auth import authenticate
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def LoginView(request):
-    if request.method == 'POST':
-        username = request.data.get('username')
-        password = request.data.get('password')
+    if request.method == "POST":
+        username = request.data.get("username")
+        password = request.data.get("password")
 
-        user = authenticate(username = username, password = password)
-
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response(
+                {"message": "User doesnot exist"}, status=status.HTTP_404_NOT_FOUND
+            )
         refresh = RefreshToken.for_user(user)
-        if user is not None:
-            return Response({
-                'is_admin' : user.is_superuser,
-                'refresh' : str(refresh),
-                'access' : str(refresh.access_)
-            })
+        return Response(
+            {
+                "is_admin": user.is_superuser,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "message": "user is logged in",
+            }
+        )
