1f89e0601e9f58c9b85486c6e9306589ad84020d
Nikita0728 <kunwornikita123@gmail.com>
2025-03-12 23:24:38 +0545

    nikita nikita

--- backend/pharmaproject/pharmaproject/settings.py
+++ backend/pharmaproject/pharmaproject/settings.py
@@ -73,28 +73,28 @@ WSGI_APPLICATION = 'pharmaproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import os
-from dotenv import load_dotenv
- 
-load_dotenv()
- 
 DATABASES = {
-     "default": {
-         "ENGINE": "django.db.backends.postgresql",
-         "NAME": os.getenv("DB_NAME"),
-         "USER": os.getenv("DB_USER"),
-         "PASSWORD": os.getenv("DB_PASSWORD"),
-         "HOST": os.getenv("DB_HOST", "localhost"),
-         "PORT": os.getenv("DB_PORT", "5432"),
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# import os
+# from dotenv import load_dotenv
+ 
+# load_dotenv()
+ 
+# DATABASES = {
+#      "default": {
+#          "ENGINE": "django.db.backends.postgresql",
+#          "NAME": os.getenv("DB_NAME"),
+#          "USER": os.getenv("DB_USER"),
+#          "PASSWORD": os.getenv("DB_PASSWORD"),
+#          "HOST": os.getenv("DB_HOST", "localhost"),
+#          "PORT": os.getenv("DB_PORT", "5432"),
+#      }
+#  }
  
 
 # Password validation
