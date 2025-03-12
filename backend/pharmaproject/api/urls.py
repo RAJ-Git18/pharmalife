0717d4d84f646c46982605c2b97aeeafa5c48340
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-03-12 23:23:54 +0545

    fix issue

--- backend/pharmaproject/api/urls.py
+++ backend/pharmaproject/api/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from login.views import LoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    # path("login/", admin.site.urls),
-
+    path("login/", LoginView),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- backend/pharmaproject/login/views.py
+++ backend/pharmaproject/login/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+@api_view(['POST'])
+def LoginView(request):
+    if request.method == 'POST':
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username = username, password = password)
+
+        refresh = RefreshToken.for_user(user)
+        if user is not None:
+            return Response({
+                'is_admin' : user.is_superuser,
+                'refresh' : str(refresh),
+                'access' : str(refresh.access_)
+            })
