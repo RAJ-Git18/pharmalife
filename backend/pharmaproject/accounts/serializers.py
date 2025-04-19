337242e1603b846dfc5da2bd3ea8971717a5b856
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-04-19 15:02:39 +0545

    backend folder deleted

--- backend/pharmaproject/accounts/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import CustomUser
-from rest_framework import serializers
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['user_id','username', 'email', 'password']
