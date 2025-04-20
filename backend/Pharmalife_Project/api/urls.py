6745e041df44c538f1b77cdd429b1b6f3973fd9d
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-04-20 09:25:08 +0545

    register url

--- backend/Pharmalife_Project/api/urls.py
+++ backend/Pharmalife_Project/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from login.views import LoginView
+from accounts.views import RegisterAPI
 
 urlpatterns = [
     # write the path name here!!
     path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterAPI.as_view(), name='register'),
 ]
