36667fcae171ed33ead00a264f8dbab3409a52fc
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-04-29 13:02:42 +0545

    cart backend

--- backend/Pharmalife_Project/cart/serializers.py
+++ backend/Pharmalife_Project/cart/serializers.py
@@ -6,12 +6,12 @@ class CartSerializer(serializers.ModelSerializer):
         model = CartModel
         fields = '__all__'
 
-        def validate(self,data):
-            cart_productid = CartModel.objects.filter(userid = data.userid,productid = data.productid).first()
+    def create(self,data):
+        cart_productid = CartModel.objects.filter(userid = data['userid'],productid = data['productid']).first()
 
-            if cart_productid is not None:
-                cart_productid.quantity = cart_productid.quantity + 1
-                cart_productid.save()
+        if cart_productid is not None:
+            cart_productid.quantity = cart_productid.quantity + 1
+            cart_productid.save()
+            return cart_productid
 
-
-            return data
+        return CartModel.objects.create(**data)
