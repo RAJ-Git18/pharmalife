4967344a282be0540bc44640b43f56bd39955da5
RAJ-Git18 <simkhadaraj98@gmail.com>
2025-05-01 19:16:52 +0545

    serializer

--- backend/Pharmalife_Project/products/serializers.py
+++ backend/Pharmalife_Project/products/serializers.py
@@ -1,23 +1,32 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(required=False, allow_null=True)
-    
+
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'stock', 'image','is_latest','is_featured']
-        extra_kwargs = {
-            'image': {'required': False}
-        }
-    
+        fields = [
+            "id",
+            "name",
+            "description",
+            "price",
+            "stock",
+            "image",
+            "is_latest",
+            "is_featured",
+        ]
+        extra_kwargs = {"image": {"required": False}}
+
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        if instance.image:
-            representation['image'] = instance.image.url
+        request = self.context.get("request")
+
+        if instance.image and hasattr(instance.image, "url"):
+            if request is not None:
+                representation["image"] = request.build_absolute_uri(instance.image.url)
+            else:
+                representation["image"] = instance.image.url
+
         return representation
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if obj.image and hasattr(obj.image, 'url'):
-            return request.build_absolute_uri(obj.image.url)
-        return None
